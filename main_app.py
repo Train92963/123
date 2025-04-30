505eccbf4ea36ab1285c9355436190f1a2032577
Train92963 <wayne92122502@gmail.com>
2025-04-30 15:26:52 +0800

    main_app.py

--- main_app.py
+++ main_app.py
@@ -15,7 +15,7 @@ import mistralai
 # Whisper 模型載入
 @st.cache_resource
 def load_whisper_model():
-    return whisper.load_model("tiny")
+    return whisper.load_model("small")
 
 # 翻譯模型載入（Helsinki-NLP 中文翻英文）
 @st.cache_resource
